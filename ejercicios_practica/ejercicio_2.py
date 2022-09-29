533fe52e7511b781544144c7f87507905e41aaeb
Mariano Andolina <mariano.3gs@gmail.com>
2022-09-28 23:21:47 -0300

    28/9 2nd commit

--- ejercicios_practica/ejercicio_2.py
+++ ejercicios_practica/ejercicio_2.py
@@ -19,18 +19,50 @@ texto_2 = str(input('Ingrese la segunda palabra:\n'))
 # Compare cual de las dos palabras es mayor (alfabéticamente)
 # Imprima en pantalla según corresponda
 
+# if texto_1 > texto_2:
+#     print(f'La palabra {texto_1} es mayor alfabrticamente.')
+# elif texto_2 > texto_1:
+#     print(f'La palabra {texto_2} es mayor alfabrticamente.')
+# else:
+#     print('Son iguales palabras')
+
+
 # Compare cual de las dos palabras tiene mayor
 # cantidad de letras
 # Imprima en pantalla según corresponda
 
+# if len(texto_1) > len(texto_2):
+#     print('La palabra "{}" tiene mas cantidad de letras que la palbra "{}"'.format(texto_1, texto_2))
+# elif len(texto_2) > len(texto_1):
+#     print('La palbra "{}" tiene mas cantidad de letras que la palabra "{}"'.format(texto_2, texto_1))
+# else:
+#     print('Las palabras son iguales.')        
+
 # Verifique si la primera letra de la primera palabra
 # es mayor a la primera letra de la segunda palabra
 # Imprima en pantalla según corresponda
+
+# if texto_1[0] > texto_2[0]:
+#     print('La primera letra de "{}" es mayor a la primera letra de "{}"'.format(texto_1, texto_2))
+# elif texto_2[0] > texto_1[0]:
+#     print('La primera letra de "{}" es mayor a la primera letra de "{}"'.format(texto_2, texto_1))
+# else:
+#     print('La primera letra de cada palabra son iguales.')
 
 copia_texto_1 = texto_1  # Copia de la variable texto_1
 
 # Verifique que copia_texto_1 es igual a texto_1
 # Imprima en pantalla según corresponda
 
+# if copia_texto_1 == texto_1:
+#     print('Las palabras son iguales')
+# else:
+#     print('Las palabras no son iguales.')
+
 # Verifique que copia_texto_1 es distinta a texto_2
 # Imprima en pantalla según corresponda
+
+if copia_texto_1 != texto_2:
+    print('Las copia de la variable texto_1 es distinta a la variable texto_2')
+else:
+    print('Las copia de la variable texto_1 es no es distinta a la variable texto_2')
