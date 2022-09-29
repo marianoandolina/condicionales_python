f6aaad4364c4c0f7bd5aca706607d4a327adbfcd
Mariano Andolina <mariano.3gs@gmail.com>
2022-09-28 22:50:14 -0300

    28/9 first commit

--- ejercicios_practica/ejercicio_1.py
+++ ejercicios_practica/ejercicio_1.py
@@ -19,20 +19,40 @@ numero_2 = int(input('Ingrese el segundo número:\n'))
 # Compare cual de los dos números es mayor
 # Imprima en pantalla según corresponda
 
-# Verifique si el numero_1 positivo, negativo o cero
-# Imprima el resultado en cada caso
-
-# Verifique si el numero_1 es mayor a 0 y menor a 100
-# Imprima en pantalla si se cumple o no la condición
-
-# Verifique si el numero_1 es menor a 10 o el numero_2
-# es mayor a -2
-# Imprima en pantalla si se cumple o no la condición
-
 if numero_1 > numero_2:
     print(f"El {numero_1} es mayor")
 elif numero_1 < numero_2:
     print(f"El {numero_2} es mayor")
 else:
     print(f"{numero_1} es igual a {numero_2}")
+
+# Verifique si el numero_1 positivo, negativo o cero
+# Imprima el resultado en cada caso
+
+if numero_1 > 0:
+    print(f'El numero {numero_1} es positivo')
+elif numero_1 == 0:
+    print('El numero es cero')
+else: 
+    print(f'El numero {numero_1} es negativo.')
+
+# Verifique si el numero_1 es mayor a 0 y menor a 100
+# Imprima en pantalla si se cumple o no la condición
+
+if numero_1 > 0 and numero_1 < 100:
+    print('Se cumple la condicion.')
+else:
+    print('No se cumple la condicion.')
+
+
+# Verifique si el numero_1 es menor a 10 o el numero_2
+# es mayor a -2
+# Imprima en pantalla si se cumple o no la condición
+
+if numero_1 < 10 or numero_2 > -2:
+    print('Se cumple la condicion')
+else:
+    print('No se cumple la condicion')    
+
+
 
