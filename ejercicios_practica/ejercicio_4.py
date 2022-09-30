eaef4dc9d10da71ce27c75d1b1c4029782507321
Mariano Andolina <mariano.3gs@gmail.com>
2022-09-29 21:47:39 -0300

    1st commit 29/9

--- ejercicios_practica/ejercicio_4.py
+++ ejercicios_practica/ejercicio_4.py
@@ -17,12 +17,26 @@ texto_2 = '7'
 # se utiliza el operador mayor o menor con Strings (textos)
 # Imprima en pantalla según corresponda
 
+if texto_1 > texto_2:
+    print('"{}" es mayor a "{}"'.format(texto_1, texto_2))
+else:
+    print('"{}" es mayor a "{}"'.format(texto_2, texto_1))    
+
 # 2-Transforma esas variables tipo texto en variables numéricas con (int)
 # y almacénalas en nuevas variables.
 # Compare las nuevas variables para ver cual es mayor o menor
 # utilizando los operadores correspondientes
 # ¿Cuál de las nuevas variables es mayor?
 # Imprima en pantalla según corresponda
+
+numero_1 = int(texto_1)
+numero_2 = int(texto_2)
+
+if numero_1 > numero_2:
+    print('El numero "{}" es mayor al numero "{}"'.format(numero_1, numero_2))
+else:
+    print('El numero "{}" es mayor al numero "{}"'.format(numero_2, numero_1))
+
 
 # Para pensar!
 # ¿Por qué cree que texto_2 es mayor a texto_1?
