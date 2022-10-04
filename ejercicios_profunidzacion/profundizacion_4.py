5a3f7ffd780064da43b601a8e1e2676dd146b44f
Mariano Andolina <mariano.3gs@gmail.com>
2022-10-03 21:23:36 -0300

    1st commit 3/10

--- ejercicios_profunidzacion/profundizacion_4.py
+++ ejercicios_profunidzacion/profundizacion_4.py
@@ -33,3 +33,42 @@ contenido.
 
 print('Ejercicios de práctica con cadenas')
 # Empezar aquí la resolución del ejercicio
+
+palabra_1 = str(input("Ingrese la primera palabra: "))
+palabra_2 = str(input("Ingrese la segunda palabra: "))
+palabra_3 = str(input("Ingrese la tercera palabra: "))
+
+cantidad_1 = len(palabra_1)
+cantidad_2 = len(palabra_2)
+cantidad_3 = len(palabra_3)   
+
+opcion = int(input("Ingrese como quiere ordenar las palabras\n 1 - Para ordenar alfabeticamente\n 2 - Para ordenar por cantidad de letras: "))
+
+if opcion == 1:
+    if palabra_1 < palabra_2 and palabra_1 < palabra_3 and palabra_2 < palabra_3:
+        print(palabra_1, palabra_2, palabra_3)
+    elif palabra_1 < palabra_2 and palabra_1 < palabra_3 and palabra_3 < palabra_2:
+        print(palabra_1, palabra_3, palabra_2)    
+    elif palabra_2 < palabra_1 and palabra_2 < palabra_3 and palabra_1 < palabra_3:
+        print(palabra_2, palabra_1, palabra_3)
+    elif palabra_2 < palabra_1 and palabra_2 < palabra_3 and palabra_3 < palabra_1:
+        print(palabra_2, palabra_3, palabra_1)    
+    elif palabra_3 < palabra_1 and palabra_3 < palabra_2 and palabra_1 < palabra_2:
+        print(palabra_3, palabra_1, palabra_2)
+    else:
+        print(palabra_3, palabra_2, palabra_1)
+
+if opcion == 2:
+    if len(palabra_1) < len(palabra_2) and len(palabra_1) < len(palabra_3) and len(palabra_2) < len(palabra_3):
+        print(palabra_1, palabra_2, palabra_3)
+    elif len(palabra_1) < len(palabra_2) and len(palabra_1) < len(palabra_3) and len(palabra_3) < len(palabra_2):
+        print(palabra_1, palabra_3, palabra_2)    
+    elif len(palabra_2) < len(palabra_1) and len(palabra_2) < len(palabra_3) and len(palabra_1) < len(palabra_3):
+        print(palabra_2, palabra_1, palabra_3)
+    elif len(palabra_2) < len(palabra_1) and len(palabra_2) < len(palabra_3) and len(palabra_3) < len(palabra_1):
+        print(palabra_2, palabra_3, palabra_1)    
+    elif len(palabra_3) < len(palabra_1) and len(palabra_3) < len(palabra_2) and len(palabra_1) < len(palabra_2):
+        print(palabra_3, palabra_1, palabra_2)
+    else:
+        print(palabra_3, palabra_2, palabra_1)
+
